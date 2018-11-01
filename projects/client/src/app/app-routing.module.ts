078f3519a563c7e13f4aad7a6d66834fbb10c804
vulcan9 <pdi1066@naver.com>
2018-11-01 18:43:17 +0900

    .

--- projects/client/src/app/app-routing.module.ts
+++ projects/client/src/app/app-routing.module.ts
@@ -10,13 +10,14 @@ const routerOption = {
 };
 
 const routes: Routes = [
+
     {
         path: '',
         // redirectTo: '/user', pathMatch: 'full',
-        // component: Page404Component
-        children: [
-            {path: '**', component: UserComponent},
-        ]
+        component: Page404Component
+        // children: [
+        //     {path: '**', component: Page404Component},
+        // ]
     },
 
     {path: 'auth', component: AuthComponent},
@@ -27,10 +28,15 @@ const routes: Routes = [
         // component: UserComponent,
         // canActivate: [AuthGuard],
         children: [
-            { path: ':id', component: UserComponent },
-            {path: '**', component: UserComponent},
+            { path: '', component: UserComponent },
+            // { path: ':id', component: UserComponent },
+            // {path: '**', component: UserComponent},
         ]
     },
+    { path: 'user/:id', component: UserComponent },
+
+    {path: '**', component: Page404Component},
+
 ];
 
 @NgModule({
@@ -38,3 +44,12 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+// https://medium.com/@ozgurgul/angular-6-login-and-router-tutorial-ac5fc5d3027f
+// https://www.a-mean-blog.com/ko/blog/Angular-2/%EA%B8%B0%EB%B3%B8%EC%82%AC%EC%9D%B4%ED%8A%B8-%EB%A7%8C%EB%93%A4%EA%B8%B0/Guard%EB%A1%9C-%EB%B9%84%EB%A1%9C%EA%B7%B8%EC%9D%B8-%EC%A0%91%EA%B7%BC%EC%A0%9C%ED%95%9C-%EB%A7%8C%EB%93%A4%EA%B8%B0
+
+// http://jasonwatmore.com/post/2018/05/16/angular-6-user-registration-and-login-example-tutorial
+// https://stackblitz.com/edit/angular-6-registration-login-example
+
+// JWT
+// https://poiemaweb.com/angular-jwt-authentication
